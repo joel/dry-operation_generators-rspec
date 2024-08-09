363c9044a79ebb42bf4349fcc0db9bd9b50608c2
Joel AZEMAR <joel.azemar@gmail.com>
2024-08-09 13:22:46 +0200

    Initial Commit

---
+++ lib/dry_operations_generators_rspec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "dry_operations_generators_rspec/version"
+
+module DryOperationsGeneratorsRspec
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/dry_operations_generators_rspec/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module DryOperationsGeneratorsRspec
+  VERSION = "0.1.0"
+end
